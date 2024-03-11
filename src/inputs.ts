c8d72c7be360b0c76c0b47a58f5e38131a248e25
lafkpages <luisafk@proton.me>
2024-03-11 10:42:37 +0100

    Use Enquirer for better inputs, optimize rotation

--- src/inputs.ts
+++
@@ -1,7 +0,0 @@
-import { homedir } from "node:os";
-import { join as joinPaths } from "node:path";
-
-const homeDir = homedir();
-
-export const filePath = joinPaths(homeDir, "Downloads/untitled.obj");
-export const modelName = "Icosphere";
