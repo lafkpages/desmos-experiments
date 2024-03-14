e8299e2d3bd1dd85dcc92c76f834619770c1321e
lafkpages <luisafk@proton.me>
2024-03-14 20:26:14 +0100

    feat: Vercel Speed Insights

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,9 @@
 import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
 
 import { dev } from "$app/environment";
 
 inject({ mode: dev ? "development" : "production" });
+injectSpeedInsights();
 
 export const prerender = true;
