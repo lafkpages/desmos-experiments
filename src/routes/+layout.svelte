1a650c43879c674660e0ffc6ead6b9378afe38af
lafkpages <luisafk@proton.me>
2024-03-19 16:12:58 +0100

    fix: put navbar in page container

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,21 +4,21 @@
   import "../app.pcss";
 </script>
 
-<Navbar fluid>
-  <NavBrand href="/">
-    <img src="/favicon.png" alt="Cube in Desmos" class="me-3 h-6 sm:h-9" />
-    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
-      >Desmos experiments</span
-    >
-  </NavBrand>
-  <NavHamburger />
-  <NavUl>
-    <NavLi href="/obj">OBJ to Desmos</NavLi>
-    <NavLi href="/img">Image to Desmos</NavLi>
-  </NavUl>
-</Navbar>
-
 <div class="flex flex-col gap-4 p-4 size-full">
+  <Navbar fluid>
+    <NavBrand href="/">
+      <img src="/favicon.png" alt="Cube in Desmos" class="me-3 h-6 sm:h-9" />
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+        >Desmos experiments</span
+      >
+    </NavBrand>
+    <NavHamburger />
+    <NavUl>
+      <NavLi href="/obj">OBJ to Desmos</NavLi>
+      <NavLi href="/img">Image to Desmos</NavLi>
+    </NavUl>
+  </Navbar>
+
   <slot />
 </div>
 
