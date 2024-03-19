7bce2170d322c61ed493239afa2fef7747d0bb08
lafkpages <luisafk@proton.me>
2024-03-19 10:07:30 +0100

    feat: navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,21 @@
 <script>
+  import { Navbar, NavBrand, NavHamburger, NavLi, NavUl } from "flowbite-svelte";
+
   import "../app.pcss";
 </script>
+
+<Navbar fluid>
+  <NavBrand href="/">
+    <img src="/favicon.png" alt="Cube in Desmos" class="me-3 h-6 sm:h-9" />
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span
+    >
+  </NavBrand>
+  <NavHamburger />
+  <NavUl>
+    <NavLi href="/obj">OBJ to Desmos</NavLi>
+    <NavLi href="/img">Image to Desmos</NavLi>
+  </NavUl>
+</Navbar>
 
 <div class="flex flex-col gap-4 p-4 size-full">
   <slot />
