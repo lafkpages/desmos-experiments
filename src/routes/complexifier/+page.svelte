1c6327c356f4d8c5acd9c9690e908ba798590a62
lafkpages <luisafk@proton.me>
2024-05-08 10:42:14 +0200

    feat: complexifier Desmos embed and input labels

--- src/routes/complexifier/+page.svelte
+++ src/routes/complexifier/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import { onMount } from "svelte";
 
-  import { Button, NumberInput } from "flowbite-svelte";
+  import { Button, Label, NumberInput } from "flowbite-svelte";
   import { MathfieldElement } from "mathlive";
 
+  import Desmos from "$components/Desmos.svelte";
   import { generate } from "$lib/crazy-desmos-expressions/src";
   import { ComputeEngine } from "$lib/crazy-desmos-expressions/src/computeEngine";
   import { latexToDesmosLatex } from "$lib/crazy-desmos-expressions/src/utils";
@@ -16,6 +17,8 @@
   let mathContainer: HTMLDivElement;
   let mathfield: MathfieldElement;
 
+  let calculator: any;
+
   let ce: ComputeEngine;
   onMount(() => {
     ce = new ComputeEngine();
@@ -24,22 +27,42 @@
     mathfield.readonly = true;
 
     mathContainer.appendChild(mathfield);
+
+    calculator.updateSettings({
+      graphpaper: false,
+      zoomButtons: false,
+    });
   });
 </script>
 
-<NumberInput bind:value={n} />
-<NumberInput bind:value={iters} />
+<div class="flex gap-6 mb-6">
+  <div class="grow">
+    <Label for="input-n" class="block mb-2">Your number</Label>
+    <NumberInput id="input-n" bind:value={n} />
+  </div>
+
+  <div class="grow">
+    <Label for="input-iters" class="block mb-2">Iterations (a.k.a. complexity)</Label>
+    <NumberInput id="input-iters" bind:value={iters} />
+  </div>
+</div>
 
 <Button
+  class="mb-2"
   on:click={() => {
     const expr = generate(n, iters);
     const box = ce.box(expr, { canonical: false });
     latex = latexToDesmosLatex(box.latex);
 
     mathfield.value = latex;
+    calculator.setExpression({
+      id: "expr",
+      latex,
+    });
   }}
 >
   Generate
 </Button>
 
-<div bind:this={mathContainer} />
+<Desmos bind:calculator />
+<div class="w-full overflow-auto" bind:this={mathContainer} />
