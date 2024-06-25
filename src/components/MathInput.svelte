87b7066c5e8e3fb3383070458edf3a599aefe424
lafkpages <luisafk@proton.me>
2024-06-25 19:21:47 +0200

    feat: MathInput component

---
+++ src/components/MathInput.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { Input } from "flowbite-svelte";
+
+  import "mathlive";
+
+  import type { MathfieldElement } from "mathlive";
+
+  export let latex = "";
+  $: if (mathfield) {
+    mathfield.value = latex;
+  }
+
+  let mathfield: MathfieldElement;
+</script>
+
+<Input {...$$props} let:props>
+  <!-- type="text" fixes styling -->
+  <math-field
+    type="text"
+    {...props}
+    class="mt-2 {props.class}"
+    bind:this={mathfield}
+    on:input={() => {
+      // TODO: readable store might be more efficient
+      latex = mathfield.value;
+    }}
+  />
+</Input>
